402028c3010c58141cb98deca395b7ad7ccd49f5
JeahaOh <jeaha1217@gmail.com>
2020-01-25 18:44:38 +0900

    MyBatid 연동, TEST.

--- README.md
+++ README.md
@@ -37,6 +37,7 @@ Maven으로 Build한 Java Spring Project의 MVC 패턴 게시판 예제.
 - JDBC Logger 설정
 - HandlerInterceptorAdapter를 상속받은 LoggerInterceptor 생성
   - Controller로 요청이 들어 올 때, 대상 URI와 Request Parameter를 출력하는데 사용할 수 있다.
+- Spring MyBatis 연동, TEST
 
 ### 20.01.20
 - 프로젝트 생성
---
+++ board/src/test/java/com/study/brd/DataSourceTest.java
@@ -0,0 +1,37 @@
+package com.study.brd;
+
+import java.sql.Connection;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/*.xml"})
+public class DataSourceTest {
+  protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @Inject
+  private DataSource dataSource;
+
+  @Test
+  public void dataSourceConnectionTest() throws Exception {
+    Connection conn = null;
+
+    try {
+      conn = dataSource.getConnection();
+      logger.info("\n\t!! connection SUCCESS: [{}]!!", conn);
+    } catch( Exception e ) {
+      logger.error("\n\t!! connection FAIL: [{}]!!", conn);
+      e.getMessage();
+    }
+  }
+}
